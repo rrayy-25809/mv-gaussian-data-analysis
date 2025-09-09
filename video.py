522a0b5b04e9486af9e6b0f5fc246ae313fc00ec
rrayy <72653431+rrayy-25809@users.noreply.github.com>
2025-09-10 02:07:43 +0900

    Changes to be committed: OpenCV 업데이트, for 문에 객체 길이를 가져오고 다시 range한 다음 인덱스로 넣는 비효율 적인 코드 제거 	modified:   video.py

--- video.py
+++ video.py
@@ -1,31 +1,32 @@
 import cv2
-import main
+from main import 사진반환 # 쓸데없이 함수 모듈을 전부 불러오지 않고 함수만 가져오기
 
 def video_to_frames(video_filename):
     vidcap = cv2.VideoCapture(video_filename)
     success, image = vidcap.read()
     images = []
-    while success:
+    while success: # 이거 아이디어 좋다
         images.append(image)
         success, image = vidcap.read()
     return images
 
 def transform_images(images):
-    return [cv2.cvtColor(main.사진반환(image), cv2.COLOR_BGR2RGB) for image in images]
+    return [cv2.cvtColor(사진반환(image), cv2.COLOR_BGR2RGB) for image in images]
 
 def frames_to_video(images, output_filename):
     height, width, layers = images[0].shape
     size = (width,height)
-    out = cv2.VideoWriter(output_filename, cv2.VideoWriter_fourcc(*'mp4v'), 30, size)
-    for i in range(len(images)):
-        out.write(images[i])
+    out = cv2.VideoWriter(output_filename, cv2.VideoWriter.fourcc(*'mp4v'), 30, size) # 4.12 버전에서는 VideoWriter 객체 안으로 함수가 이동됨
+    for i in images:
+        out.write(i)
     out.release()
 
-#사진변환
-images = video_to_frames('ex.mp4')
+if __name__ == "__main__":
+    #사진반환
+    images = video_to_frames('video.mp4')
 
-#사진변형
-transformed_images = transform_images(images)
+    #사진변형
+    transformed_images = transform_images(images)
 
-#사진조립
-frames_to_video(transformed_images, 'output.mp4')
+    #사진조립
+    frames_to_video(transformed_images, 'output.mp4')
