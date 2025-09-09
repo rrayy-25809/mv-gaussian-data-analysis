f0bc6a576c5fdcf83514ba4cd2269730f41bb063
rrayy <72653431+rrayy-25809@users.noreply.github.com>
2025-09-10 02:06:01 +0900

    Changes to be committed: GMM 함수 제거, img_process 함수로 메인 구조 간략화 	modified:   main.py

--- main.py
+++ main.py
@@ -4,11 +4,6 @@ import cv2
 import matplotlib.pyplot as plt
 from scipy.stats import multivariate_normal
 
-def 가우시안적합(coords):
-    gmm = GaussianMixture(n_components=1, covariance_type='full').fit(coords)
-    mean = gmm.means_[0]
-    return mean
-
 def 픽셀선택(mean): #사진에 표기를 위한 함수
     max_density_x = mean[0]
     max_density_y = mean[1]
@@ -22,7 +17,7 @@ def 색칠(img_rgb, start_x, end_x, start_y, end_y): #사진에 표기를 위한
     img_rgb[start_y:end_y, start_x:end_x] = [255, 0, 0]
     return img_rgb
 
-def 사진출력(img_rgb): #배열 -> 사진 변환
+def 사진저장(img_rgb): #배열 -> 사진으로 저장 (사진출력이란 이름은 배열을 사진으로 보여주는 것으로 오해할 수 있음)
     cv2.imwrite('output.jpg', cv2.cvtColor(img_rgb, cv2.COLOR_RGB2BGR))
 
 def G값시각화(mask):
@@ -46,59 +41,47 @@ def 가우시안분포생성(mean, cov, mask): #가우시안 분포: 2차원 정
     plt.contour(X, Y, Z)
     plt.show()
 
-def 사진반환(img): #표기된 사진 배열 생성
-    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    R, G, B = cv2.split(img_rgb)
+def image_process(img):
+    """이미지 처리 및 GMM 적합을 수행"""
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # 이미지의 색상 공간을 변환
+    R, G, B = cv2.split(img_rgb) # RGB 분리
     mask = np.where(((G - np.maximum(R, B)) > 50) & (G > np.maximum(R, B)), 1, 0)
-    y_indices, x_indices = np.where(mask == 1)
-    coords = np.column_stack((x_indices, y_indices))
-    mean = 가우시안적합(coords)
-    start_x, end_x, start_y, end_y = 픽셀선택(mean)
-    img_rgb = 색칠(img_rgb, start_x, end_x, start_y, end_y)
+
+    y_indices, x_indices = np.where(mask == 1) # 값이 1인것만 뽑아내기
+    coords = np.column_stack((x_indices, y_indices)) # 2차원 배열로 변환
+
+    # GMM적합 (굳이 GMM 적합을 두번 할 필요가 없음, 함수를 제거하여 중복되는 코드 삭제)
+    gmm = GaussianMixture(n_components=1, covariance_type='full').fit(coords)
+    # 평균/공분산 계산
+    mean = gmm.means_[0] # type: ignore
+    cov = gmm.covariances_[0] # type: ignore
+    
+    return img_rgb, mask, mean, cov
+
+def 사진반환(img): #표기된 사진 배열 생성
+    img_rgb, _, mean, _ = image_process(img)
+    
+    start_x, end_x, start_y, end_y = 픽셀선택(mean) # 밀도가 가장 높은 픽셀 선택
+    img_rgb = 색칠(img_rgb, start_x, end_x, start_y, end_y) # 빨간색으로 표시
     return img_rgb
 
-# 이미지
-img = cv2.imread('image.jpg')
+if __name__ == "__main__": # 위 조건문이 없으면 import 할 때도 실행될 수 있음 만약을 위해 써주기
+    img = cv2.imread('image.jpg') # 이미지
 
-# RGB값 저장
-img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    if img is not None:
+        img_rgb, mask, mean, cov = image_process(img)
 
-# R, G, B 분리
-R, G, B = cv2.split(img_rgb)
-# G에서 R과 B 중 더 큰 값을 뺀 값이 50 이상이고, RGB중 G가 가장 큰것은 1로, 아니면 0으로 배열생성
-mask = np.where(((G - np.maximum(R, B)) > 50) & (G > np.maximum(R, B)), 1, 0)
+        start_x, end_x, start_y, end_y = 픽셀선택(mean) # 밀도가 가장 높은 픽셀 선택
+        img_rgb = 색칠(img_rgb, start_x, end_x, start_y, end_y) # 빨간색으로 표시
 
-# 값이 1인것만 뽑아내기
-y_indices, x_indices = np.where(mask == 1)
+        가우시안분포생성(mean, cov, mask)# 가우시안 분포 시각화
 
-# 2차원 배열로 변환
-coords = np.column_stack((x_indices, y_indices))
+        # mask 값 시각화
+        G값시각화(mask)
 
-# GMM 적합 및 밀도가 가장 큰 픽셀의 좌표 계산
-mean = 가우시안적합(coords)
+        plt.imshow(img_rgb)
+        plt.show() #사진출력
 
-# 밀도가 가장 높은 픽셀 선택
-start_x, end_x, start_y, end_y = 픽셀선택(mean)
-
-# 빨간색으로 표시
-img_rgb = 색칠(img_rgb, start_x, end_x, start_y, end_y)
-
-# 변경된 이미지 저장
-사진출력(img_rgb)
-
-
-
-# GMM적합
-gmm = GaussianMixture(n_components=1, covariance_type='full').fit(coords)
-# 평균/공분산 계산
-mean = gmm.means_[0]
-cov = gmm.covariances_[0]
-# 가우시안 분포 시각화
-가우시안분포생성(mean, cov, mask)
-
-# mask 값 시각화
-G값시각화(mask)
-
-#사진출력
-plt.imshow(img_rgb)
-plt.show()
+        사진저장(img_rgb) # 변경된 이미지 저장
+    else:
+        raise Exception("입력한 이미지를 찾을 수 없습니디.")
